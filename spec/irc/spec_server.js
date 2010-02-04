e2bd7679a881aa75226ae39277885c67edcd441b
Craig R Webster <craig@barkingiguana.com>
2010-02-04 14:45:05 +0000

    Make sure the server listens on port 6667.

--- spec/irc/spec_server.js
+++ spec/irc/spec_server.js
@@ -2,11 +2,23 @@ var IRC     = require(path.join(process.cwd(), 'lib', 'irc', 'server')),
     Server = IRC.Server;
 
 describe("An IRC server", function() {
-  beforeEach(function() {
+  it("should listen on port 6667 by default", function() {
+    var tcp             = require("tcp"),
+        checkedForError = false,
+        server          = new Server();
+    server.start();
 
-  });
+    var client = tcp.createConnection(6667);
+    client.setTimeout(1000);
+    client.addListener("connect", function() {
+      client.close();
+    });
+    client.addListener("close", function(error) {
+      checkedForError = true;
+      assert(!error);
+    });
 
-  it("should ...", function() {
-    // ...
+    setTimeout(function() { server.stop() }, 1000);
+    setTimeout(function() { assert(checkedForError) }, 1500);
   });
 })
