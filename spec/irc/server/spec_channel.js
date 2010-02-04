8744f122fac337b6414e5e2bddbe05ca0f73ee4b
Craig R Webster <craig@barkingiguana.com>
2010-02-04 14:03:14 +0000

    Ensure a channel has a name.

--- spec/irc/server/spec_channel.js
+++ spec/irc/server/spec_channel.js
@@ -3,7 +3,13 @@ var IRC     = require(path.join(process.cwd(), 'lib', 'irc', 'server')),
 
 describe("An IRC channel", function() {
   it("should have a name", function() {
-    channel = new Channel("nodejs");
+    var channel = new Channel("nodejs");
     assertEqual("nodejs", channel.name());
+  });
+
+  it("should require a name to be specified at creation", function() {
+    assertThrow("A channel needs a name", function() {
+      var channel = new Channel();
+    });
   });
 })
